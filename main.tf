adf4e1e132705d3f0550d9c98c8392b15039122a
hc <hc@localhost.localdomain>
2025-12-07 15:33:48 +0100

    Fixed issues with apply and destroy flags

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = ">= 1.2"
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.71.0"
+    }
+  }
   cloud {
 
     organization = "ACME-Segregated-Environments"
@@ -6,17 +13,15 @@ terraform {
       name = "acme-resource-manager"
     }
   }
-
 }
+
 module "workspace" {
+  source   = "./modules/tfe-workspace"
   for_each = var.workspaces_to_deploy
-  source  = "app.terraform.io/ACME-Segregated-Environments/ACME-workspace-controller-module/tfc"
-  version = "1.0.0"
 
   github_organization = each.value.github_organization
   tfe_organization    = each.value.tfe_organization
-  workspace_settings  = each.value.workspace_settings
   oauth_token_id      = var.oauth_token_id
 
+  workspace_settings = each.value.workspace_settings
 }
-
--- modules/tfe-workspace/variables.tf
+++ modules/tfe-workspace/variables.tf
@@ -8,35 +8,30 @@ variable "tfe_organization" {
   description = "Terraform Cloud organization"
 }
 
-variable "workspaces_to_deploy" {
-  type = map(object({
-    github_organization = string
-    tfe_organization    = string
-    workspace_settings = object({
-      name                           = string
-      auto_destroy_activity_duration = optional(string)
-      vcs_repo = object({
-        branch     = string
-        identifier = string
-      })
-      apply = optional(object({
-        manual_confirm    = optional(bool)
-        wait_for_run      = optional(bool)
-        retry_attempts    = optional(number)
-        retry_backoff_min = optional(number)
-      }))
-      destroy = optional(object({
-        manual_confirm    = optional(bool)
-        wait_for_run      = optional(bool)
-        retry_attempts    = optional(number)
-        retry_backoff_min = optional(number)
-      }))
-    })
-  }))
-}
-
-
 variable "oauth_token_id" {
   type      = string
   sensitive = true
+}
+
+variable "workspace_settings" {
+  type = object({
+    name                           = string
+    auto_destroy_activity_duration = optional(string)
+    vcs_repo = object({
+      branch     = string
+      identifier = string
+    })
+    apply = optional(object({
+      manual_confirm    = optional(bool)
+      wait_for_run      = optional(bool)
+      retry_attempts    = optional(number)
+      retry_backoff_min = optional(number)
+    }))
+    destroy = optional(object({
+      manual_confirm    = optional(bool)
+      wait_for_run      = optional(bool)
+      retry_attempts    = optional(number)
+      retry_backoff_min = optional(number)
+    }))
+  })
 }
